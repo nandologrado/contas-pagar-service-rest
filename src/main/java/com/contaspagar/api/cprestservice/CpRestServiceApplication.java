774de790aad253958abaa1261e6bb60f8dc3f011
Fernando Maciel Logrado <nando.maciel@gmai.com>
2020-03-09 23:22:21 -0300

    Criando a aplicação

---
+++ src/main/java/com/contaspagar/api/cprestservice/CpRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.contaspagar.api.cprestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contaspagar/api/cprestservice/CpRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contaspagar.api.cprestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
